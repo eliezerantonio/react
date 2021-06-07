8f54da4cb282891ebf985238e872c0c51a311000
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-07 22:18:25 +0100

    map

--- array.js
+++ array.js
@@ -1,1 +1,6 @@
-const 
+const precos = ['Credito', '200 KZ', '400 KZ', 'Contas a pagar', '300 KZ', '400 KZ'];
+
+
+const precosFiltro = precos.filter((preco) => preco.includes('KZ'))
+
+const precos= numeros =precosFiltro.map((preco)=> preco.replace('KZ', ''))
