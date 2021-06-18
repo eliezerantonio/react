61aa453ed1ed180090d4af6b45b779e452af6e2a
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-18 23:00:59 +0100

    mano ta cuiar

--- 02-react-iniciantes/05-npx-create-app/meuapp/src/App.js
+++ 02-react-iniciantes/05-npx-create-app/meuapp/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 
+const random = Math.random();
+const activo = true;
+
+function mostrarNome(sobrenome) {
+  return "Eliezer " + sobrenome;
+}
 
 const App = () => {
-    return <div> Meu primeiro app React</div>;
-
-
+  return (
+    <>
+      {mostrarNome("Antonio")}
+      <p className={activo ? "activo" : "Inativi"}>{random * 100}</p>
+    </>
+  );
 };
 
 export default App;
