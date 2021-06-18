fe1f855b146aa319a95b934d7b9c7a16d7ebede7
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-19 00:19:35 +0100

    arrays 2 done

--- 02-react-iniciantes/arrays/src/App.js
+++ 02-react-iniciantes/arrays/src/App.js
@@ -1,4 +1,5 @@
-// import React from "react";
+/* eslint-disable no-unused-vars */
+import React from "react";
 
 // const App = () => {
 //   const animes = ["One Piece", "Attack Titan", "Jujutsu Kaisen", "Dragon Ball"];
@@ -12,28 +13,48 @@
 //   );
 // };
 
-// export default App;
+// const App = () => {
+//   const books = [
+//     { nome: "One Piece", ano: 1992 },
+//     { nome: "Attack Titan", ano: 2001 },
+//     { nome: "Jujutsu Kaisen", ano: 2020 },
+//   ];
 
-import React from "react";
+//   return (
+//     <ul>
+//       {books
+//         .filter(({ ano }) => ano >= 1998)
+//         .map(({ nome, ano }) => (
+//           <li key={nome}>
+//             {nome}, {ano}
+//           </li>
+//         ))}
+//     </ul>
+//   );
+// };
+
+const produtos = [
+  {
+    id: 1,
+    nome: "Smartphone",
+    preco: "R$ 2000",
+    cores: ["#29d8d5", "#252a34", "#fc3766"],
+  },
+  {
+    id: 2,
+    nome: "Notebook",
+    preco: "R$ 3000",
+    cores: ["#ffd045", "#d4394b", "#f37c59"],
+  },
+  {
+    id: 3,
+    nome: "Tablet",
+    preco: "R$ 1500",
+    cores: ["#365069", "#47c1c8", "#f95786"],
+  },
+];
 
 const App = () => {
-  const books = [
-    { nome: "One Piece", ano: 1992 },
-    { nome: "Attack Titan", ano: 2001 },
-    { nome: "Jujutsu Kaisen", ano: 2020 },
-  ];
-
-  return (
-    <ul>
-      {books
-        .filter(({ ano }) => ano >= 1998)
-        .map(({ nome, ano }) => (
-          <li key={nome}>
-            {nome}, {ano}
-          </li>
-        ))}
-    </ul>
-  );
+  return <></>;
 };
-
 export default App;
