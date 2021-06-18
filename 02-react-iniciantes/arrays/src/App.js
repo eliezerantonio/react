b294601cc305b97053283faf595517827771d3e3
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-19 00:01:36 +0100

    arryas

---
+++ 02-react-iniciantes/arrays/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => {
+  const produtos = [<li></li>, <li></li>, <li></li>];
+
+  console.log(produtos);
+  return <div></div>;
+};
+
+export default App;
