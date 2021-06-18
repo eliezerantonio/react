0aa3ae1c53240bc523cdcf2ce51c77e8a5e91d68
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-19 00:15:33 +0100

    filtro

--- 02-react-iniciantes/arrays/src/App.js
+++ 02-react-iniciantes/arrays/src/App.js
@@ -1,13 +1,37 @@
+// import React from "react";
+
+// const App = () => {
+//   const animes = ["One Piece", "Attack Titan", "Jujutsu Kaisen", "Dragon Ball"];
+
+//   return (
+//     <ul>
+//       {animes.map((filme) => (
+//         <li key={filme}>{filme}</li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+// export default App;
+
 import React from "react";
 
 const App = () => {
-  const animes = ["One Piece", "Attack Titan", "Jujutsu Kaisen", "Dragon Ball"];
+  const books = [
+    { nome: "One Piece", ano: 1992 },
+    { nome: "Attack Titan", ano: 2001 },
+    { nome: "Jujutsu Kaisen", ano: 2020 },
+  ];
 
   return (
     <ul>
-      {animes.map((filme) => (
-        <li key={filme}>{filme}</li>
-      ))}
+      {books
+        .filter(({ ano }) => ano >= 1998)
+        .map(({ nome, ano }) => (
+          <li key={nome}>
+            {nome}, {ano}
+          </li>
+        ))}
     </ul>
   );
 };
