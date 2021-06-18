ff61b7ae6dde9e7e38af9dd3a98ea65b3597de97
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-18 23:29:37 +0100

    somando

--- 02-react-iniciantes/05-npx-create-app/meuapp/src/App.js
+++ 02-react-iniciantes/05-npx-create-app/meuapp/src/App.js
@@ -1,23 +1,52 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 
-const random = Math.random();
-const activo = true;
+const luana = {
+  cliente: "Luana2",
 
-const estiloP = {
+  idade: 27,
+
+  compras: [
+    { nome: "Notbook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+    { nome: "Guitara", preco: "R$ 3500" },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: "Mario",
+
+  idade: 31,
+
+  compras: [
+    { nome: "Notbook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+    { nome: "Guitara", preco: "R$ 3500" },
+  ],
+  ativa: false,
+};
+
+const styleBlue = {
   color: "blue",
 };
 
-function mostrarNome(sobrenome) {
-  return "Eliezer " + sobrenome;
-}
-
+const styleRed = {
+  color: "red",
+};
 const App = () => {
+  const dados = mario;
+  const total = dados.compras.map((item) => item.preco.replace("R$ ",""));
+
+  console.log(total);
+
   return (
     <>
-      {mostrarNome("Antonio")}
-      <p style={estiloP} className={activo ? "activo" : "Inativi"}>
-        {random * 100}
-      </p>
+      <p>Nome: {dados.cliente}</p>
+      <p>Idade: {dados.idade}</p>
+      <p>Situacao: </p>
     </>
   );
 };
