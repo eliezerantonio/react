1541b922638c483cb9791eb3c86d463c008743f7
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-19 01:01:35 +0100

    juntando componentes

--- 02-react-iniciantes/arrays/src/Form.js
+++ 02-react-iniciantes/arrays/src/Form.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Button from "./Button";
+import Input from "./Input";
 
 const Form = () => {
-    return <div>
-      
-  </div>;
+  return (
+    <form>
+      <p>
+        <label htmlFor="nome">Nome</label>
+      </p>
+
+      <p>
+        <label htmlFor="email">E-mail</label>
+
+        <Input />
+      </p>
+      <Button />
+    </form>
+  );
 };
 
 export default Form;
