f3c070b8f1ee595d88ce1bc0283da73364d7346f
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-20 11:47:47 +0100

    chilred

--- 02-react-iniciantes/props/src/App.js
+++ 02-react-iniciantes/props/src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 
-const Titulo = ({cor, texto}) => {
-  return <h1 style={{ color: cor }}>{texto}</h1>;
+const Titulo = ({ cor, texto, children }) => {
+  return (
+    <h1 style={{ color: cor }}>
+      {texto},{children}
+    </h1>
+  );
 };
 
 const App = () => {
   return (
     <>
       <div>
-        <Titulo cor="red" texto="Meu Titulo 1" />
+        <Titulo cor="red" texto="Meu Titulo 1">
+        
+        </Titulo>
         <Titulo cor="blue" texto="Meu Titulo 1" />
       </div>
     </>
