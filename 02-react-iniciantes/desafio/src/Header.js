8c8385bb9a466ef46a9ee07899ea252238c72951
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-20 19:29:55 +0100

    create componets

---
+++ 02-react-iniciantes/desafio/src/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <Header>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/produtos">Produtos</a>
+          </li>
+        </ul>
+      </Header>
+    </div>
+  );
+}
+
+export default Header;
---
+++ 02-react-iniciantes/desafio/src/Produtos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Produtos() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Produtos
