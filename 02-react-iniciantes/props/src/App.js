d75b1b170a6842afaad826c98093ab52e4703b2a
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-20 11:44:33 +0100

    porps

--- 02-react-iniciantes/props/src/App.js
+++ 02-react-iniciantes/props/src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const App() = () => {
-  
+const Titulo = ({cor, texto}) => {
+  return <h1 style={{ color: cor }}>{texto}</h1>;
+};
+
+const App = () => {
   return (
-
     <>
-      
-      </>
+      <div>
+        <Titulo cor="red" texto="Meu Titulo 1" />
+        <Titulo cor="blue" texto="Meu Titulo 1" />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
