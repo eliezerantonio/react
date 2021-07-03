7e32216ddde9cea52c1478e1915fefa858794841
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-03 01:13:55 +0100

    agora

--- 03-React-hooks/usestate/src/App.js
+++ 03-React-hooks/usestate/src/App.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import ButtonModal from "./ButtonModal";
+import Modal from "./Modal";
 
 function App() {
   const [modal, setModal] = React.useState(false);
   return (
     <div>
+      <Modal modal={modal} setModal={setModal} />
       <div>{modal ? "Modal Aberto" : "Modal Fechado"}</div>
       <ButtonModal setModal={setModal} />
     </div>
