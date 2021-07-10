d56c92327f8f0ff598425c7e631932f9db2cd54e
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-10 19:29:04 +0100

    useREF

--- 03-React-hooks/use_ref/src/App.js
+++ 03-React-hooks/use_ref/src/App.js
@@ -1,13 +1,63 @@
+// import React from "react";
+
+// const App = () => {
+//   const [comentarios, setComentarios] = React.useState(["Teste", "teste2"]);
+
+//   const [input, setInput] = React.useState();
+
+//   const inputElement = React.useRef();
+
+//   function handleClick() {
+//     setComentarios([...comentarios, input]);
+//     setInput("");
+//     inputElement.current.focus();
+//   }
+//   return (
+//     <div>
+//       <ul>
+//         {comentarios.map((comentario, i) => (
+//           <li key={i}>{comentario}</li>
+//         ))}
+//       </ul>
+//       <input
+//         type="text"
+//         ref={inputElement}
+//         value={input}
+//         onChange={({ target }) => setInput(target.value)}
+//       />
+//       <button onClick={handleClick}>Enviar</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
 import React from "react";
 
 const App = () => {
-  const video = React.useRef();
+  const [carrinho, setCarrinho] = React.useState(0);
 
-  React.useEffect(() => {
-    console.log(video.current.currentTime);
-  });
-  React.useEffect(() => {});
-  return <div ref={video}>Teste</div>;
+  const [notificacao, setNotificacao] = React.useState(null);
+
+  const timeoutRef = React.useRef();
+
+  function handleClick() {
+
+    setCarrinho(carrinho + 1);
+
+    setNotificacao("Item adicionado ao carinho");
+
+    clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() => {
+      setNotificacao(null);
+    }, 2000);
+  }
+  return (
+    <div>
+      <p>{notificacao}</p>
+      <button onClick={handleClick}>Adicionar ao carrinho {carrinho}</button>
+    </div>
+  );
 };
 
 export default App;
