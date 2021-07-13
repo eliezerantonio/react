bb19c6fc9f18dcb92a25306930d744536c55ced5
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-14 00:52:56 +0100

    handleChange

--- 04-Formularios/input_exercicio/src/App.js
+++ 04-Formularios/input_exercicio/src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
-const App = () => {
-  return <div>
 
-    
-  </div>;
+const App = () => {
+  const [form, setForm] = React.useState({
+    nome: "",
+    email: "",
+    senha: "",
+    cep: "",
+    rua: "",
+    bairro: "",
+    cidade: "",
+    estado: "",
+  });
+
+  function handleChange({ target }) {
+    const { id, value } = target;
+
+    setForm({ ...form, [id]: value });
+  }
+  return (
+    <div>
+      <label htmlFor="nome">Nome</label>
+      <input type="text" id="nome" value={form.nome} onChange={handleChange} />
+    </div>
+  );
 };
 
 export default App;
