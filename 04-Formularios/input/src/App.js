f0a22902660f5ccdfa6712c8161672d9ebfed57c
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-14 00:04:51 +0100

    reactividade input

--- 04-Formularios/input/src/App.js
+++ 04-Formularios/input/src/App.js
@@ -1,9 +1,43 @@
-const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import React from "react";
 
-export default App
+const App = () => {
+  const [form, setForm] = React.useState({ nome: "", email: "" });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+  }
+
+  function handleChange({ target }) {
+    const { id, value } = target;
+
+    console.log(id, value);
+
+    setForm({ ...form, [id]: value });
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="nome">Nome</label>
+      <input
+        id="nome"
+        type="text"
+        name="nome"
+        value={form.nome}
+        onChange={handleChange}
+      />
+      {form.nome} <br />
+      <label>E-mail</label>
+      <input
+        id="email"
+        type="email"
+        name="email"
+        value={form.email}
+        onChange={handleChange}
+      />
+      {form.email}
+      <button>Enviar</button>
+    </form>
+  );
+};
+
+export default App;
