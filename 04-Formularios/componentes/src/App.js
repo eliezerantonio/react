cc1ea0d2e24c682f29e7f3e5cb36a6837dac03bb
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-16 22:23:55 +0100

    inout

---
+++ 04-Formularios/componentes/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Input from "./Form/Input";
+const App = () => {
+  return (
+    <form>
+      <Input />
+    </form>
+  );
+};
+
+export default App;
---
+++ 04-Formularios/componentes/src/Form/Input.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <div>
+      <label htmlFor="">Nome</label>
+      <input type="text" />
+    </div>
+  );
+};
+
+export default Input;
