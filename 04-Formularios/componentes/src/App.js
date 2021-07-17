ed763a4626abdb354c6b06ca9579ad84cb213931
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-18 00:36:12 +0100

    input componentre

--- 04-Formularios/componentes/src/App.js
+++ 04-Formularios/componentes/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Input from "./Form/Input";
 const App = () => {
+  const [nome, setNome] = React.useState("");
+  const [email, setEmail] = React.useState("");
   return (
     <form>
-      <Input label="Nome" id="nome" />
-      <Input label="E-mail" id="email" />
+      <Input label="Nome" id="nome" value={nome} setValue={setNome} />
+      <Input
+        label="E-mail"
+        id="email"
+        value={email}
+        setValue={setEmail}
+        required
+      />
+      <button>Enviar</button>
     </form>
   );
 };
--- 04-Formularios/componentes/src/Form/Input.js
+++ 04-Formularios/componentes/src/Form/Input.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-const Input = ({ label , id }) => {
+const Input = ({ label, id, setValue, ...props }) => {
   return (
-    <div>
-      <label htmlFor="">{label}</label>
-      <input type="text" />
-    </div>
+    <>
+      <label htmlFor={id}>{label}</label>
+      <input
+        type="text"
+        id={id}
+        name={id}
+        onChange={({ target }) => setValue(target.value)}
+        {...props}
+      />
+    </>
   );
 };
 
