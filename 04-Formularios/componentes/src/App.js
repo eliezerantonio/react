8a4103c8c2f185f14aa1695e24c69b37dba50e78
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-18 01:11:31 +0100

    componente select

--- 04-Formularios/componentes/src/App.js
+++ 04-Formularios/componentes/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Input from "./Form/Input";
+import Select from "./Form/Select";
 const App = () => {
   const [nome, setNome] = React.useState("");
   const [email, setEmail] = React.useState("");
+
+  const [produto, setProduto] = React.useState("");
   return (
     <form>
+      <Select
+        options={["tablet", "watch"]}
+        value={produto}
+        setValue={setProduto}
+      />
+      
       <Input label="Nome" id="nome" value={nome} setValue={setNome} />
       <Input
         label="E-mail"
