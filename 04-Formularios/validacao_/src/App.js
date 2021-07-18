10359811e0d24c3cd60dc4ae16b5b65625e9bd1a
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-18 22:57:35 +0100

    validando error

--- 04-Formularios/validacao_/src/App.js
+++ 04-Formularios/validacao_/src/App.js
@@ -2,10 +2,23 @@ import React from "react";
 import Input from "./Form/Input";
 const App = () => {
   const [cep, setCep] = React.useState("");
+  const [error, setError] = React.useState(null);
+
+  function validateCep(value) {
+    if (value.length === 0) {
+      setError("Preencha um valor");
+      return false;
+    } else if (!/^\d{5}-?\d{3}$/.test(value)) {
+      setError("Preencha um valor valido");
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
 
   function handleBlur({ target }) {
-    const regex = /^\d{5}-?\d{3}$/;
-    const validacao = regex.test(target.value);
+    const validacao = validateCep(target.value);
 
     console.log(validacao);
   }
@@ -20,7 +33,7 @@ const App = () => {
         onBlur={handleBlur}
         placeholder="00000-000"
       />
-      {cep}
+      {error && <p>error</p>}
     </form>
   );
 };
