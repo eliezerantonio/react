90b1b42aeb73a83d203c27e315fc03297f13738d
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-18 23:48:45 +0100

    refatrando

---
+++ 04-Formularios/validacao_/src/Hooks/userForm.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const types = {
+  cep: {
+    regex: /^\d{5}-?\d{3}$/,
+    message: "Cep invalido",
+  },
+};
+
+const useForm = (type) => {
+  const [value, setValue] = React.useState("");
+  const [error, setError] = React.useState(null);
+
+  function validate() {
+    if (value.length === 0) {
+      setError("Preencha um valor");
+      return false;
+    } else if (!types[type].test(value)) {
+      setError(!types[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+
+  function onChange({ target }) {
+    if (error) validate(target.value);
+    setValue(target.value);
+  }
+  return {
+    value,
+    setValue,
+    error,
+    onChange,
+    onBlur: () => validate(value),
+  };
+};
+
+export default useForm;
