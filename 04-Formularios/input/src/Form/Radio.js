8e8122b32702a87995aeb29c5a123909f75713f0
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-22 20:39:35 +0100

    stilizando

--- 04-Formularios/input/src/Form/Radio.js
+++ 04-Formularios/input/src/Form/Radio.js
@@ -1,9 +1,24 @@
 import React from "react";
 
-const Radio = () => {
+const Radio = ({ pergunta, options, id }) => {
   return (
-    <fieldset>
-      <legend>Nome da pergunta</legend>
+    <fieldset
+      style={{
+        padding: "2rem",
+        marginBotton: "1rem",
+        border: "2px solid #eee",
+      }}
+    >
+      <legend style={{ fontWeight: "bold" }}>{pergunta}</legend>
+      {options.map((option) => (
+        <label
+          key={option}
+          style={{ marginBotton: "1rem", fontFamily: "monospace" }}
+        >
+          <input type="radio" value={option} />
+          {option}
+        </label>
+      ))}
     </fieldset>
   );
 };
