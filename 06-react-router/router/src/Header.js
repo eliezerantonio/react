7e33a68d6ce56b25f3d91fe7896303e1bb9e3130
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-28 11:51:40 +0100

    navegacao

--- 06-react-router/router/src/Header.js
+++ 06-react-router/router/src/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 const Header = () => {
   return (
-    <div>
-      <h3>Essa e a header</h3>
-    </div>
+    <nav>
+      <NavLink to="/" end>
+        Home
+      </NavLink>{" "}
+      | <NavLink to="sobre">Sobre</NavLink>| <NavLink to="login">Login</NavLink>
+    </nav>
   );
 };
 
