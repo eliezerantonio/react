4544b2cdad861e016c52c62666cfe0b9c28b3b4c
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-28 06:12:39 +0100

    contrucao de paginas

--- 06-react-router/router/src/App.js
+++ 06-react-router/router/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Sobre from "./Sobre";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="sobre" element={<Sobre />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ 06-react-router/router/src/Sobre.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Sobre = () => {
+  return (
+    <div>
+      <h1>Sobre</h1>
+      <p> Pagina sobre</p>
+    </div>
+  );
+};
+
+export default Sobre;
