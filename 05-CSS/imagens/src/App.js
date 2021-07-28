8ce89d6bd32afc8ed6cce26afa2cf52940c2aff8
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-28 05:30:46 +0100

    svg

--- 05-CSS/imagens/src/App.js
+++ 05-CSS/imagens/src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
-import foto from "./img/foto.jpg";
+// import foto from "./img/foto.jpg";
 import "./App.css";
 import DogSvg from "./DogSvg";
 
 const App = () => {
+  const [olho, setOlho] = React.useState(0);
+
+  function aument() {
+    setOlho(olho + 1);
+  }
+
   return (
     <div>
-      <p className="fundo"></p>
-      <DogSvg />
-      <img src={foto} alt="Cachorro" />
+      <p onClick={aument} className="fundo"></p>
+      <DogSvg color="#84e" olho={olho} />
+      {/* <img src={foto} alt="Cachorro" /> */}
     </div>
   );
 };
--- 05-CSS/imagens/src/DogSvg.js
+++ 05-CSS/imagens/src/DogSvg.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const DogSvg = ({ color }) => {
+const DogSvg = ({ color, olho }) => {
   return (
     <svg
       width="28"
@@ -13,8 +13,8 @@ const DogSvg = ({ color }) => {
         d="M14 10h1.652c1.708 0 2.63 2.004 1.518 3.302l-1.618 1.887A4.501 4.501 0 0024.5 14.5a1.5 1.5 0 013 0A7.5 7.5 0 0114 19 7.5 7.5 0 01.5 14.5a1.5 1.5 0 013 0 4.5 4.5 0 008.948.689l-1.618-1.887C9.718 12.004 10.64 10 12.35 10H14z"
         fill={color}
       />
-      <circle cx="21" cy="3" r="3" fill={color} />
-      <circle cx="7" cy="3" r="3" fill={color} />
+      <circle cx="21" cy={olho} r={olho} fill={color} />
+      <circle cx="7" cy={olho} r={olho} fill={color} />
     </svg>
   );
 };
