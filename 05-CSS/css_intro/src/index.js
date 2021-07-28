088cf2ecaa5a89bcfc7023093cf8d0d0d2cd268d
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-28 02:57:08 +0100

    importando bootstrap

--- 05-CSS/css_intro/src/index.js
+++ 05-CSS/css_intro/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.js";
 ReactDOM.render(
   <React.StrictMode>
     <App />
