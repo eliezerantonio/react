cfb09fbb6d71ac5abf4ce84020647eab4778d31d
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-28 05:49:25 +0100

    routes

---
+++ 06-react-router/router/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
