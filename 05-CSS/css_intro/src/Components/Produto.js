5336a5015c65433fdd905efe827368194a6880e9
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-28 02:50:33 +0100

    rotacionameto botao

--- 05-CSS/css_intro/src/Components/Produto.js
+++ 05-CSS/css_intro/src/Components/Produto.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import styles from "./Produto.module.css";
 const Produto = () => {
   return (
     <div>
-      <h1>Notebook</h1>
-      <p>R$ 20000</p>
-      <button>Comprar</button>
+      <h1 className={styles.titulo}>Notebook</h1>
+      <p className={styles.preco}> R$ 20000</p>
+      <button className={styles.comprar}>Comprar</button>
     </div>
   );
 };
