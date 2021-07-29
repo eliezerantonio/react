c37b91eb4687e62dff3adde4738ad07ca40ef242
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-29 23:34:09 +0100

    j

--- 06-react-router/router/src/Home.js
+++ 06-react-router/router/src/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+import { NavLink } from "react-router-dom";
+
 const Home = () => {
   return (
     <div>
       <p>Pagina Home</p>
+      <NavLink to="produto/notebook">Notebook</NavLink>
+      <NavLink to="produto/smartphone">Smartphone</NavLink>
     </div>
   );
 };
