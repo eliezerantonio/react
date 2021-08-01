8ffc711d29b158cee8db5b96bc88b37700210f25
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-01 02:19:12 +0100

    mostrando prpdutos

--- 06-react-router/router/src/Produto.js
+++ 06-react-router/router/src/Produto.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+import { useParams } from "react-router-dom";
 const Produto = () => {
+  const params = useParams();
+  
   return (
     <div>
-      <h1>Produto</h1>
+      <h1>Produto {params.id}</h1>
     </div>
   );
 };
