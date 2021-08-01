07a93e3986571f20349f3830550eaf50b91e790f
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-01 04:33:52 +0100

    produot.js, header.js, footer.js

--- 06-react-router/desafio/src/App.js
+++ 06-react-router/desafio/src/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Produtos from "./Components/Produtos";
 
 const App = () => {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Produtos></Produtos>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ 06-react-router/desafio/src/Components/Produtos.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Produtos.module.css";
+const Produtos = () => {
+  return (
+    <div>
+      <h1>Produtos</h1>
+    </div>
+  );
+};
+
+export default Produtos;
