a62834f1840f3487ce9f5ad841389e28c1540667
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-01 02:33:40 +0100

    use location

--- 06-react-router/router/src/Produto.js
+++ 06-react-router/router/src/Produto.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 const Produto = () => {
   const params = useParams();
-  
+
+  const location = useLocation();
+
+  const search = new URLSearchParams(location.search);
+  console.log(search.get("key"));
   return (
     <div>
       <h1>Produto {params.id}</h1>
