e592c6f5b033ba5166e70436bc823d6157a7cb8d
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-02 01:04:18 +0100

    animando contacto

--- 06-react-router/desafio/src/Components/Contacto.js
+++ 06-react-router/desafio/src/Components/Contacto.js
@@ -1,10 +1,20 @@
 import React from "react";
 import styles from "./Contacto.module.css";
+
+import foto from "../img/contato.jpg";
 const Contacto = () => {
   return (
-    <div>
-      <h1>Contacto</h1>
-    </div>
+    <section className={styles.contacto}>
+      <img src={foto} alt="Maquina de escrever" />
+      <div>
+        <h1>Entre em contacto</h1>
+        <ul className={styles.dados}>
+          <li>eliezer@origamid.com </li>
+          <li>924033375 </li>
+          <li>Bairro Cdt Cow-Boy </li>
+        </ul>
+      </div>
+    </section>
   );
 };
 
