ac31574c377971c5457e10c6921c9f23c695d159
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-02 01:36:06 +0100

    buscando produtos

--- 06-react-router/desafio/src/Components/Produtos.js
+++ 06-react-router/desafio/src/Components/Produtos.js
@@ -1,10 +1,26 @@
 import React from "react";
 import styles from "./Produtos.module.css";
+
+import { Link } from "react-router-dom";
+
 const Produtos = () => {
+  const [produtos, setProdutos] = React.useState(null);
+  React.useEffect(() => {
+    fetch("https://ranekapi.origamid.dev/json/api/produto")
+      .then((r) => r.json())
+      .then((json) => setProdutos(json));
+  }, []);
+
+  if (produtos === null) return null;
   return (
-    <div>
-      <h1>Produtos</h1>
-    </div>
+    <section className={styles.produtos + " animeLeft"}>
+      {produtos.map((produto) => (
+        <Link to={`produto/${produto.id}`} key={produto.id}>
+          <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo} />{" "}
+          <h1>{produto.nome}</h1>
+        </Link>
+      ))}
+    </section>
   );
 };
 
