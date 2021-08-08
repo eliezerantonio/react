7ebec6362db673f938c95cfd102ecf274a9f2083
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-08 23:02:43 +0100

    referenciando  class login

--- projecto_final/src/Components/Header.js
+++ projecto_final/src/Components/Header.js
@@ -1,14 +1,17 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
-
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 const Header = () => {
   return (
     <div className={styles.header}>
-      <nav className="container">
-        {" "}
-        <Link to="/">Home</Link>
-        <Link to="/login">Login/ ciar</Link>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login/ ciar
+        </Link>
       </nav>
     </div>
   );
