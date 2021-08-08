33dd988e27c62e89f5df6fe471288dd6df6e9af9
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-08 23:19:05 +0100

    chamando componentes

--- projecto_final/src/Components/Login/Login.js
+++ projecto_final/src/Components/Login/Login.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import LoginCreate from "./LoginCreate";
 import LoginForm from "./LoginForm";
+import LoginPasswordLost from "./LoginPasswordLost";
+import LoginPasswordReset from "./LoginPasswordReset";
 
 const Login = () => {
   return (
     <div>
       <Routes>
-        <Route path="/login" component={<LoginForm />} />
-        <Route path="/login" component={<LoginForm />} />
-        <Route path="/login" component={<LoginForm />} />
-        <Route path="/login" component={<LoginForm />} />
+        <Route path="/" component={<LoginForm />} />
+        <Route path="criar" component={<LoginCreate />} />
+        <Route path="perdeu" component={<LoginPasswordLost />} />
+        <Route path="resetar" component={<LoginPasswordReset />} />
       </Routes>
     </div>
   );
