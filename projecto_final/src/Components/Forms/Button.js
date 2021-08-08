da21373fd8b8a36dfd829664f17692ace5c3a430
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-08 23:47:03 +0100

    module

---
+++ projecto_final/src/Components/Forms/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./Button.module.css";
+const Button = () => {
+  return <button className={styles.button}>entrar</button>;
+};
+
+export default Button;
