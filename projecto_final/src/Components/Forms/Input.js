2f1f08ca5d1f68945ad5106d23de81dca75aaf82
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-08 23:53:52 +0100

    input

--- projecto_final/src/Components/Forms/Input.js
+++ projecto_final/src/Components/Forms/Input.js
@@ -1,7 +1,12 @@
 import React from "react";
-import styles from "Input.module.css";
-const Input = () => {
-  return <input className={styles.input} />;
+import styles from "./Input.module.css";
+const Input = ({ label, type, name }) => {
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <input id={name} name={name} className={styles.input} type={type} />
+    </div>
+  );
 };
 
 export default Input;
--- projecto_final/src/Components/Login/LoginForm.js
+++ projecto_final/src/Components/Login/LoginForm.js
@@ -1,30 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "../Forms/Button";
+import Input from "../Forms/Input";
 
 const LoginForm = () => {
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
 
-    function handleSubmit (event) {
-        event.preventDefault();
-        fetch('http')
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch("http");
   }
   return (
     <section>
       Login form
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={username}
-          onChange={({ target }) => setUsername(target.value)}
-        />{" "}
-        <input
-          type="password"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-        />
-        <button>Entrar</button>
+        <Input label="Usuario" type="text" name="username" />
+        <Input label="Senha" type="password" name="password" />
+       <Button/>
       </form>
       <Link to="/login/criar">Cadastros</Link>
     </section>
