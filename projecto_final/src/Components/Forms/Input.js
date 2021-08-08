b1f62d0bcec1e35de9692cfa63e592969caf799c
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-09 00:47:31 +0100

    validando

--- projecto_final/src/Components/Forms/Input.js
+++ projecto_final/src/Components/Forms/Input.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Input.module.css";
-const Input = ({ label, type, name, value, onChange }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
@@ -13,8 +13,9 @@ const Input = ({ label, type, name, value, onChange }) => {
         type={type}
         onChange={onChange}
         value={value}
+        onBlur={onBlur}
       />
-      <p className={styles.error}>Error</p>
+      {error && <p className={styles.error}>Error</p>}
     </div>
   );
 };
--- projecto_final/src/Components/Login/LoginForm.js
+++ projecto_final/src/Components/Login/LoginForm.js
@@ -1,26 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from "../Forms/Button";
 import Input from "../Forms/Input";
-import useForm from "../../Hooks/userForm";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
+
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  async function handleSubmit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
-    fetch("http");
+
+    if (username.validate() && password.validate()) {
+      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(),
+      })
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((json) => {
+          console.log(json);
+        });
+    }
   }
+
   return (
     <section>
-      Login form
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuario" type="text" name="username" {...username} />
+        <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
-      <Link to="/login/criar">Cadastros</Link>
+      <Link to="/login/criar">Cadastro</Link>
     </section>
   );
 };
