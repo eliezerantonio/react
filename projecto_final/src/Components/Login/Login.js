4ad4ec1e4839857a19e0a8823f95121a402a3523
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-08 23:16:18 +0100

    componentes

--- projecto_final/src/Components/Login/Login.js
+++ projecto_final/src/Components/Login/Login.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import LoginForm from "./LoginForm";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" component={<LoginForm />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Login;
