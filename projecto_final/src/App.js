28b8d0a03413b4a119eaab57b962be1d312701d0
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-08 22:09:06 +0100

    css apps

--- projecto_final/src/App.js
+++ projecto_final/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import "./App.css";
 
 const App = () => {
   return (
@@ -11,6 +13,7 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- projecto_final/src/Components/Header.js
+++ projecto_final/src/Components/Header.js
@@ -1,8 +1,17 @@
 import React from "react";
 import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div className={styles.header}>Header</div>;
+  return (
+    <div className={styles.header}>
+      <nav className="container">
+        {" "}
+        <Link to="/">Home</Link>
+        <Link to="/login">Login/ ciar</Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
